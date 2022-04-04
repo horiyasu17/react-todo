e5e790a3a242dcf4ebd80868262ddf64011a68e4
Yasuhiro Hori <horiyasu17@users.noreply.github.com>
2022-04-05 06:38:51 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { InputTodo } from "./components/InputTodo";
+import { IncompleteTodos } from "./components/IncompleteTodos";
+import { CompleteTodos } from "./components/CompleteTodos";
+
+import "./styles.css";
+
+const App = () => {
+  const [todoText, setTodoText] = useState("");
+  const [incompleteTodos, setInconpleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+
+    const newTodos = [...incompleteTodos, todoText];
+    setInconpleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setInconpleteTodos(newTodos);
+  };
+
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+    setInconpleteTodos(newIncompleteTodos);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+  };
+
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+    setCompleteTodos(newCompleteTodos);
+
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setInconpleteTodos(newIncompleteTodos);
+  };
+
+  return (
+    <>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
+      />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録は5個まで</p>
+      )}
+      <IncompleteTodos
+        todos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodos todos={completeTodos} onClickBack={onClickBack} />
+    </>
+  );
+};
+export default App;
---
+++ src/components/CompleteTodos.jsx
@@ -0,0 +1,19 @@
+export const CompleteTodos = (props) => {
+  const { todos, onClickBack } = props;
+
+  return (
+    <div className="complete-area">
+      <p className="title">完了のTODO</p>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <li key={todo}>
+              <span>{todo}</span>
+              <button onClick={() => onClickBack(index)}>戻す</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
